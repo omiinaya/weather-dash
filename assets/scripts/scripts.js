2c717553eb28cc9823b29fa8f7881ceaf9a5300b
Omar Minaya <omiinaya@gmail.com>
2020-02-03 19:47:18 -0800

    added function to get locationid.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,10 +1,16 @@
 var d = new Date();
 var month = d.getMonth()+1
 var nd = month+"/"+d.getDate()+"/"+ d.getYear()
+var locationId = 0;
+var locName ="";
+var queryURL ="";
+
 function getUserInput() {
-  var locationId = $("#user-input").val()
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+locationId+"&appid=5495b7f13ab5bdde931c6f4d218ed2e4"
-  if (locationId) {
+  locName = $("#user-input").val()
+  queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+locName+"&appid=5495b7f13ab5bdde931c6f4d218ed2e4"
+
+  if (locName) {
+    getLocationData() 
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -18,4 +24,12 @@ function getUserInput() {
       $("#uv").text("UV Index: "+response.wind.speed)
     });
   }
+}
+function getLocationData() {
+  for (var i=0; i<jsonData.length; i++) {
+    if (jsonData[i].name == locName) {
+      locationId = jsonData[i].id;
+    }
+  }
+  console.log(locationId);
 }
