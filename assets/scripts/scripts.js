63b4148ee853cedbcafc49762c4fcbdd065a4e8f
Omar Minaya <omiinaya@gmail.com>
2020-02-26 01:25:47 -0800

    added enter key, pass data function, preparing for icons and default data.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,21 +1,27 @@
-var locId = "";
-var locName = "";
-var fixedName = "";
-var locLon = "";
-var locLat = "";
+var locId;
+var locName;
+var fixedName;
+var locLon;
+var locLat;
+var foreData;
+var uvData;
 
-var dt = "";
-var dt2 = "";
-var dt3 = "";
-var dt4 = "";
-var dt5 = "";
-var date = "";
-var date2 = "";
-var date3 = "";
-var date4 = "";
-var date5 = "";
+var dt;
+var dt2;
+var dt3;
+var dt4;
+var dt5;
+var date;
+var date2;
+var date3;
+var date4;
+var date5;
 
 var arrHistory = [];
+
+function pageLoad() {
+  enterKey()
+}
 
 function getUserInput() {
   //gets user input and capitalizes the first letter of each word.
@@ -27,9 +33,9 @@ function getUserInput() {
   getLocationId()
 
   //API URLs crafted using the coordinates and location ids we got from before.
-  queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?id="+locId+"&APPID=5495b7f13ab5bdde931c6f4d218ed2e4"
-  queryURL2 = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat="+locLat+"&lon="+locLon+"&APPID=5495b7f13ab5bdde931c6f4d218ed2e4"
-  
+  queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?id=" + locId + "&APPID=5495b7f13ab5bdde931c6f4d218ed2e4"
+  queryURL2 = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + locLat + "&lon=" + locLon + "&APPID=5495b7f13ab5bdde931c6f4d218ed2e4"
+
   //run our forecast, uv, and history functions.
   ajaxForecast()
   ajaxUV()
@@ -40,23 +46,23 @@ function getUserInput() {
 
 //capitalizes the first letter of each word provided by the user.
 function fixInput() {
-    fixedName = locName.split(' ').map(eachWord=>
-      eachWord.charAt(0).toUpperCase() + eachWord.slice(1)
-    ).join(' ');
+  fixedName = locName.split(' ').map(eachWord =>
+    eachWord.charAt(0).toUpperCase() + eachWord.slice(1)
+  ).join(' ');
 }
 
 //removes hours from the date provided by the API.
 function fixDate() {
-  date1 = dt1.substring(0,10);
-  date2 = dt2.substring(0,10);
-  date3 = dt3.substring(0,10);
-  date4 = dt4.substring(0,10);
-  date5 = dt5.substring(0,10);
+  date1 = dt1.substring(0, 10);
+  date2 = dt2.substring(0, 10);
+  date3 = dt3.substring(0, 10);
+  date4 = dt4.substring(0, 10);
+  date5 = dt5.substring(0, 10);
 }
 
 //pulls coordinates from our data.js file.
 function getCoordinates() {
-  for (var i=0; i<jsonData.length; i++) {
+  for (var i = 0; i < jsonData.length; i++) {
     if (jsonData[i].name == fixedName) {
       locLon = jsonData[i].coord.lon;
       locLat = jsonData[i].coord.lat;
@@ -66,7 +72,7 @@ function getCoordinates() {
 
 //pulls locationids from our data.js file.
 function getLocationId() {
-  for (var i=0; i<jsonData.length; i++) {
+  for (var i = 0; i < jsonData.length; i++) {
     if (jsonData[i].name == fixedName) {
       locId = jsonData[i].id;
     }
@@ -74,70 +80,92 @@ function getLocationId() {
 }
 
 //gets forecast data from the API in JSON format.
-function ajaxForecast(){
+function ajaxForecast() {
   if (locId) {
     $.ajax({
       url: queryURL1,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response);
-      $("#cityName").text(fixedName)
-      //pull dt_txts from each index to use in fixDate()
-      dt1 = response.list[0].dt_txt;
-      dt2 = response.list[8].dt_txt;
-      dt3 = response.list[16].dt_txt;
-      dt4 = response.list[24].dt_txt;
-      dt5 = response.list[32].dt_txt;
-      fixDate()
-      //day1
-      $("#date1").text("Date: "+date1)
-      $("#temperature1").text("Temperature: "+response.list[0].main.temp)
-      $("#humidity1").text("Humidity: "+response.list[0].main.humidity)
-      $("#speed1").text("Wind Speed: "+response.list[0].wind.speed)
-      //day2
-      $("#date2").text("Date: "+date2)
-      $("#temperature2").text("Temperature: "+response.list[8].main.temp)
-      $("#humidity2").text("Humidity: "+response.list[8].main.humidity)
-      $("#speed2").text("Wind Speed: "+response.list[8].wind.speed)
-      //day3
-      $("#date3").text("Date: "+date3)
-      $("#temperature3").text("Temperature: "+response.list[16].main.temp)
-      $("#humidity3").text("Humidity: "+response.list[16].main.humidity)
-      $("#speed3").text("Wind Speed: "+response.list[16].wind.speed)
-      //day4
-      $("#date4").text("Date: "+date4)
-      $("#temperature4").text("Temperature: "+response.list[24].main.temp)
-      $("#humidity4").text("Humidity: "+response.list[24].main.humidity)
-      $("#speed4").text("Wind Speed: "+response.list[24].wind.speed)
-      //day5
-      $("#date5").text("Date: "+date5)
-      $("#temperature5").text("Temperature: "+response.list[32].main.temp)
-      $("#humidity5").text("Humidity: "+response.list[32].main.humidity)
-      $("#speed5").text("Wind Speed: "+response.list[32].wind.speed)
+      foreData = response;
+      passData();
     });
   }
 }
 
 //gets UV data from API in JSON format.
-function ajaxUV(){
+function ajaxUV() {
   if (locName) {
     $.ajax({
       url: queryURL2,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response)
-      $("#uv1").text("UV Index: "+response[0].value)
-      $("#uv2").text("UV Index: "+response[1].value)
-      $("#uv3").text("UV Index: "+response[2].value)
-      $("#uv4").text("UV Index: "+response[3].value)
-      $("#uv5").text("UV Index: "+response[4].value)
+      uvData = response;
     });
   }
+}
+
+function passData() {
+  $("#cityName").text(fixedName)
+  //pull dt_txts from each index to use in fixDate()
+  dt1 = foreData.list[0].dt_txt;
+  dt2 = foreData.list[8].dt_txt;
+  dt3 = foreData.list[16].dt_txt;
+  dt4 = foreData.list[24].dt_txt;
+  dt5 = foreData.list[32].dt_txt;
+  fixDate()
+  //day1
+  $("#date1").text("Date: " + date1)
+  $("#temperature1").text("Temperature: " + foreData.list[0].main.temp)
+  $("#humidity1").text("Humidity: " + foreData.list[0].main.humidity)
+  $("#speed1").text("Wind Speed: " + foreData.list[0].wind.speed)
+  //day2
+  $("#date2").text("Date: " + date2)
+  $("#temperature2").text("Temperature: " + foreData.list[8].main.temp)
+  $("#humidity2").text("Humidity: " + foreData.list[8].main.humidity)
+  $("#speed2").text("Wind Speed: " + foreData.list[8].wind.speed)
+  //day3
+  $("#date3").text("Date: " + date3)
+  $("#temperature3").text("Temperature: " + foreData.list[16].main.temp)
+  $("#humidity3").text("Humidity: " + foreData.list[16].main.humidity)
+  $("#speed3").text("Wind Speed: " + foreData.list[16].wind.speed)
+  //day4
+  $("#date4").text("Date: " + date4)
+  $("#temperature4").text("Temperature: " + foreData.list[24].main.temp)
+  $("#humidity4").text("Humidity: " + foreData.list[24].main.humidity)
+  $("#speed4").text("Wind Speed: " + foreData.list[24].wind.speed)
+  //day5
+  $("#date5").text("Date: " + date5)
+  $("#temperature5").text("Temperature: " + foreData.list[32].main.temp)
+  $("#humidity5").text("Humidity: " + foreData.list[32].main.humidity)
+  $("#speed5").text("Wind Speed: " + foreData.list[32].wind.speed)
+  //UV info
+  $("#uv1").text("UV Index: " + uvData[0].value)
+  $("#uv2").text("UV Index: " + uvData[1].value)
+  $("#uv3").text("UV Index: " + uvData[2].value)
+  $("#uv4").text("UV Index: " + uvData[3].value)
+  $("#uv5").text("UV Index: " + uvData[4].value)
+}
+
+function testAlert() {
+  alert("test");
+}
+
+function enterKey(){
+  var enterText = document.getElementById("user-input");
+  enterText.addEventListener("keyup", function(event) {
+    if (event.keyCode===13) {
+      event.preventDefault();
+      locName = $("#user-input").val();
+      getUserInput()
+    }
+  });
 }
 
 //updates history and places it on the left side of the page.
 function updateHistory() {
   arrHistory = arrHistory.concat(fixedName);
   console.log(arrHistory)
-  $("#updateHistory").append("<li>"+fixedName)
+  $("#updateHistory").append("<li onclick='testAlert()'>" + fixedName)
 }
